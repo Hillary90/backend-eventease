beba9ec522cad61f036f332183d46724e17e53f0
Allan Ratemo <allanisratemo@gmail.com>
2025-12-03 21:49:26 +0300

    Remove unnecessary lines

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,3 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, events, bookings  # They will exist later
-
 api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
-api_router.include_router(events.router, prefix="/events", tags=["Events"])
-api_router.include_router(bookings.router, prefix="/bookings", tags=["Bookings"])
