9b038f085b6126c01db38c62baeb16831c16b2e4
AlexMureti <alexmkmaingi@gmail.com>
2025-12-06 11:34:57 +0300

    feature 4-event-management

---
+++ models/event.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+import datetime
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    organizer_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    organizer = relationship("User", back_populates="events")
--- models/user.py
+++ models/user.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel
-from typing import Optional
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db.database import Base
 
-class User(BaseModel):
-    id: str
-    name: str
-    email: str
-    password: Optional[str] = None
-    password: Optional[str] = None  
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=True)
+
+    # Event relationship
+    events = relationship("Event", back_populates="organizer")
